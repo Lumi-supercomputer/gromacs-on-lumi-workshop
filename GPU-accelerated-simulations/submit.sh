08408962a04ba79a52bc1a63c46ad16fafb4a7ba
xprasanna <59515618+xaprasanna@users.noreply.github.com>
2024-01-09 14:15:37 +0200

    updated submit.sh

--- GPU-accelerated-simulations/submit.sh
+++ GPU-accelerated-simulations/submit.sh
@@ -15,24 +15,5 @@ module load gromacs/2023.3-gpu
 
 export OMP_NUM_THREADS=7
 
-export MPICH_GPU_SUPPORT_ENABLED=1
-export GMX_ENABLE_DIRECT_GPU_COMM=1
-export GMX_FORCE_GPU_AWARE_MPI=1
-#export GMX_GPU_PME_DECOMPOSITION=1
-#export GMX_PMEONEDD=1
 
-cat << EOF > select_gpu
-#!/bin/bash
-
-export ROCR_VISIBLE_DEVICES=\$((SLURM_LOCALID%SLURM_GPUS_PER_NODE))
-exec \$*
-EOF
-
-chmod +x ./select_gpu
-
-CPU_BIND="mask_cpu:fe000000000000,fe00000000000000"
-CPU_BIND="${CPU_BIND},fe0000,fe000000"
-CPU_BIND="${CPU_BIND},fe,fe00"
-CPU_BIND="${CPU_BIND},fe00000000,fe0000000000"
-
-srun --cpu-bind=$CPU_BIND ./select_gpu gmx_mpi mdrun -s pme_nvt.tpr -bonded gpu -nb gpu -pme gpu -update gpu -g output.log -npme 1 
+srun gmx_mpi mdrun -s pme_nvt.tpr -bonded cpu -nb gpu -pme cpu -update cpu -g output.log -nsteps -1 -notunepme
