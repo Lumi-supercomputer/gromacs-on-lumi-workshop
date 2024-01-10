0025512ba97948be610bf56865388bd29ca84cef
xprasanna <59515618+xaprasanna@users.noreply.github.com>
2024-01-10 17:32:33 +0200

    Update submit.sh

--- Exercise-3.1/submit.sh
+++ Exercise-3.1/submit.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
-#SBATCH --job-name=stmv
-#SBATCH --partition=standard-g
+#SBATCH --partition=small-g
+#SBATCH --exclusive
 #SBATCH --account=project_462000002
-#SBATCH --output=stmv.out
-#SBATCH --error=stmv.err 
-#SBATCH --time=00:05:00
-#SBATCH --nodes=2
-#SBATCH --gpus-per-node=8
-#SBATCH --ntasks-per-node=8
+#SBATCH --time=00:10:00
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=2
+#SBATCH --ntasks-per-node=2
 
 module use /appl/local/csc/modulefiles
 module load gromacs/2023.3-gpu
-#module load gromacs/2023.1-heffte
 
 export OMP_NUM_THREADS=7
-
-#export MPICH_GPU_SUPPORT_ENABLED=1
-#export GMX_ENABLE_DIRECT_GPU_COMM=1
-#export GMX_FORCE_GPU_AWARE_MPI=1
-#export GMX_GPU_PME_DECOMPOSITION=1
-#export GMX_PMEONEDD=1
 
 cat << EOF > select_gpu
 #!/bin/bash
@@ -35,4 +26,4 @@ CPU_BIND="${CPU_BIND},fe0000,fe000000"
 CPU_BIND="${CPU_BIND},fe,fe00"
 CPU_BIND="${CPU_BIND},fe00000000,fe0000000000"
 
-srun --cpu-bind=$CPU_BIND ./select_gpu gmx_mpi mdrun -s pme_nvt.tpr -bonded gpu -nb gpu -pme gpu -update gpu -g output.log -npme 1
+srun --cpu-bind=$CPU_BIND ./select_gpu gmx_mpi mdrun -s pme_nvt.tpr -bonded cpu -nb gpu -pme gpu -update cpu -g ex3.1_${SLURM_JOB_ID}.log -npme 1 -nsteps -1 -maxh 0.1 -resethway -notunepme
