c59be39f30257607ba65da855a56f37cdce7a11f
xprasanna <59515618+xaprasanna@users.noreply.github.com>
2024-01-16 10:00:38 +0200

    submit script

---
+++ Exercise-4.1/Aquaporin/submit.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --partition=FIXME
+#SBATCH --account=FIXME
+#SBATCH --reservation=FIXME
+#SBATCH --exclusive
+#SBATCH --time=00:10:00
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=8
+#SBATCH --ntasks-per-node=32
+
+module use /appl/local/csc/modulefiles
+module load gromacs/2023.3-gpu
+source ${GMXBIN}/lumi-affinity.sh
+
+export OMP_NUM_THREADS=1
+
+export MPICH_GPU_SUPPORT_ENABLED=1
+export GMX_ENABLE_DIRECT_GPU_COMM=1
+export GMX_FORCE_GPU_AWARE_MPI=1
+
+srun --cpu-bind=${CPU_BIND} ./select_gpu \
+     gmx_mpi mdrun -multidir repl_{01..32} \
+                   -nb gpu \
+                   -pme gpu \
+                   -bonded gpu \
+                   -update gpu \
+                   -nsteps -1 \
+                   -maxh 0.1 \
+                   -resethway \
+                   -notunepme \
+                   -g ex4.1_id${SLURM_JOB_ID}.log
