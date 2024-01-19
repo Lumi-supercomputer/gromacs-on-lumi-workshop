1006687979fc7eb337d48beb4710ac35e2868efc
Rasmus Kronberg <rasmus.kronberg@csc.fi>
2024-01-19 15:23:42 +0200

    update 4.2 multi log naming

--- Exercise-4.2/Aquaporin/submit.sh
+++ Exercise-4.2/Aquaporin/submit.sh
@@ -18,9 +18,11 @@ export MPICH_GPU_SUPPORT_ENABLED=1
 export GMX_ENABLE_DIRECT_GPU_COMM=1
 export GMX_FORCE_GPU_AWARE_MPI=1
 
+num_multi=16
+
 srun --cpu-bind=${CPU_BIND} ./select_gpu \
      gmx_mpi mdrun -multidir sim_{01..16} \
                    -npme 1 \
                    -nb gpu -pme gpu -bonded gpu -update gpu \
-                   -g ex4.2_${SLURM_NTASKS}x${OMP_NUM_THREADS}_jID${SLURM_JOB_ID} \
+                   -g ex4.2_${SLURM_NNODES}N_multi${num_multi}_jID${SLURM_JOB_ID} \
                    -nsteps -1 -maxh 0.017 -resethway -notunepme
